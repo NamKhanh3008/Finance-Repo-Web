36e62190f6ad148b95b7eff63a3b6f8cb235c4d2
Your Name <you@example.com>
2026-01-11 01:28:32 -0500

    fix

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -34,37 +34,37 @@ def create_app():
     cache.init_app(app)
 
     # Import Models
-    from app.models import User, File, Folder
+    from .models import User, File, Folder
 
     # ------------------------------------------------------------
     # REGISTER BLUEPRINTS
     # ------------------------------------------------------------
     
     # 1. File Routes
-    from app.routes.file_routes import file_bp
+    from .routes.file_routes import file_bp
     app.register_blueprint(file_bp)
     
     # 2. Auth Routes
-    from app.routes.auth_routes import auth_bp
+    from .routes.auth_routes import auth_bp
     app.register_blueprint(auth_bp)
 
     # 3. Stock Routes (The Engine)
-    from app.routes.stock_routes import stock_bp
+    from .routes.stock_routes import stock_bp
     app.register_blueprint(stock_bp)
 
     # 4. Search Routes (REMOVED for now)
-    from app.routes.search_routes import search_bp
+    from .routes.search_routes import search_bp
     app.register_blueprint(search_bp)
 
     # 5. Annotation Routes (FIXED)
-    from app.routes.annotation_routes import annotation_bp
+    from .routes.annotation_routes import annotation_bp
     # You must add url_prefix="/api/annotations" here!
     app.register_blueprint(annotation_bp, url_prefix="/api/annotation")
 
     
 
     #6. Fork Routes
-    from app.routes.fork_routes import fork_bp
+    from .routes.fork_routes import fork_bp
     app.register_blueprint(fork_bp, url_prefix="/api/fork")
 
     return app
